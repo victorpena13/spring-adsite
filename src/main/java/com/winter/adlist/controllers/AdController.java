07576c6d6cba52b7ca4b7bacc4c08043637ffc4a
victorpena13 <victor.pena1392@gmail.com>
2020-12-24 16:41:50 -0600

    created pathway on for category page

---
+++ src/main/java/com/winter/adlist/controllers/AdController.java
@@ -0,0 +1,36 @@
+package com.winter.adlist.controllers;
+
+import com.winter.adlist.models.Ad;
+
+import com.winter.adlist.models.Category;
+import com.winter.adlist.repositories.AdRepository;
+import com.winter.adlist.repositories.CategoryRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;;
+
+import javax.servlet.annotation.WebServlet;
+
+@Controller
+public class AdController {
+
+    private final CategoryRepository categoryDao;
+    private final AdRepository adDao;
+
+    public AdController(CategoryRepository categoryRepository, AdRepository adRepository) {
+        categoryDao = categoryRepository;
+        adDao = adRepository;
+    }
+
+//creates path for categories to display corresponding posts
+    @GetMapping("/categories/{id}")
+    public String showPosts(@PathVariable long id, Model vModel) {
+        Category categoryId = categoryDao.findOne(id);
+        Iterable <Ad> ads = adDao.findAllByCategoriesOrderByIdDesc(categoryId);
+        vModel.addAttribute("category", categoryId);
+        vModel.addAttribute("ads", ads);
+        return "site/category";
+    }
+
+}
