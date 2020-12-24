c7e7b95476628b720df84980623f3e5671523ee7
victorpena13 <victor.pena1392@gmail.com>
2020-12-24 09:35:04 -0600

    updated categories are displayed through index controller that loads homepage

--- src/main/java/com/winter/adlist/controllers/IndexController.java
+++ src/main/java/com/winter/adlist/controllers/IndexController.java
@@ -1,13 +1,29 @@
 package com.winter.adlist.controllers;
 
+import com.winter.adlist.models.Category;
+import com.winter.adlist.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryDao;
+
+    public IndexController(CategoryRepository cateogoryRepository) {
+        categoryDao = cateogoryRepository;
+    }
+
+//returns homePage & pulls categories from database
     @GetMapping(value = {"/"})
-    public String homePage() {
+    public String homePage(Model vModel) {
+        Iterable<Category> categories = categoryDao.findAll();
+        vModel.addAttribute("categories", categories);
         return "index";
     }
+
 }
+
+
+
