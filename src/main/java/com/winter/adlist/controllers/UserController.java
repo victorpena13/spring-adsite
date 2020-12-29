a6f619f6b0ddab7601b758118c7467ac6dcdb531
victorpena13 <victor.pena1392@gmail.com>
2020-12-28 19:55:01 -0600

    added register contoller and html page

--- src/main/java/com/winter/adlist/controllers/UserController.java
+++ src/main/java/com/winter/adlist/controllers/UserController.java
@@ -1,4 +1,16 @@
 package com.winter.adlist.controllers;
 
+import com.winter.adlist.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/register")
+    public String signUp(Model vModel){
+        vModel.addAttribute("user", new User());
+        return "user/register";
+    }
 }
