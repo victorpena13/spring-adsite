42083936f4a9550615c0e79a4cb41d7b5471e71e
victorpena13 <victor.pena1392@gmail.com>
2021-01-05 10:50:03 -0600

    cleaned code

--- src/main/java/com/winter/adlist/controllers/UserController.java
+++ src/main/java/com/winter/adlist/controllers/UserController.java
@@ -4,15 +4,16 @@ import com.winter.adlist.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/register")
-    public String signUp(Model vModel){
+    public String registerPage(Model vModel){
         vModel.addAttribute("user", new User());
         return "user/register";
     }
-
 
 }
---
+++ src/main/java/com/winter/adlist/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.winter.adlist.repositories;
+
+import com.winter.adlist.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findUserByEmail(String email);
+}
