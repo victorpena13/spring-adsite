db57e375e7c8aea2f15b0a96d43bed8ed13f523f
victorpena13 <victor.pena1392@gmail.com>
2021-01-06 20:24:16 -0600

    granting user ability to create and post advertisements

--- src/main/java/com/winter/adlist/controllers/AdController.java
+++ src/main/java/com/winter/adlist/controllers/AdController.java
@@ -39,4 +39,10 @@ public class AdController {
         return "site/ad";
 
     }
+
+
+
+
+
+
 }
--- src/main/java/com/winter/adlist/controllers/UserController.java
+++ src/main/java/com/winter/adlist/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.winter.adlist.controllers;
 
+import com.winter.adlist.models.Ad;
+import com.winter.adlist.models.Category;
 import com.winter.adlist.models.User;
+import com.winter.adlist.repositories.CategoryRepository;
 import com.winter.adlist.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -14,14 +17,17 @@ public class UserController {
 
     private final UserRepository userDao;
     private PasswordEncoder passwordEncoder;
+    private final CategoryRepository categoryDao;
 
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, CategoryRepository categoryRepository) {
         userDao = userRepository;
         this.passwordEncoder = passwordEncoder;
+        categoryDao = categoryRepository;
     }
 
 
-    //Gets the user input
+    //retrieves page for user to input new credentials
     @GetMapping("/register")
     public String registerPage(Model vModel){
         vModel.addAttribute("user", new User());
@@ -32,7 +38,6 @@ public class UserController {
     @PostMapping("/register")
     public String saveUser(@ModelAttribute User user, Model vModel) {
         vModel.addAttribute("alreadyExists", false);
-
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         if (user.getPhoto().equalsIgnoreCase("")){
@@ -48,6 +53,15 @@ public class UserController {
         }
         userDao.save(user);
         return "redirect:/login";
+    }
+
+    //provides users a category to place the ad they are about to create
+    @GetMapping("/ads/create")
+    public String createAdForm(Model vModel) {
+        Iterable<Category> categories = categoryDao.findAll();
+        vModel.addAttribute("categories", categories);
+        vModel.addAttribute("posts", new Ad());
+        return "user/createAd";
     }
 
 }
