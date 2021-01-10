9ad39ce49ba17ed62182f9c61c8a2c6ed4d2eb11
victorpena13 <victor.pena1392@gmail.com>
2021-01-10 09:37:25 -0600

    cleaned code

--- src/main/java/com/winter/adlist/controllers/UserController.java
+++ src/main/java/com/winter/adlist/controllers/UserController.java
@@ -11,8 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class UserController {
@@ -54,8 +53,4 @@ public class UserController {
         vModel.addAttribute("posts", new Ad());
         return "user/createAd";
     }
-
-
-
-
 }
